6e318ed86f8ac34d7213b352b8280b761bb9fa70
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-03-18 13:11:25 +0600

    project name changed

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ const hotelCollection = client.db("stayFinder").collection("hotels");
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
